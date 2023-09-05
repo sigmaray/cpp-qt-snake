5843dc653c7024a4a7ba54502434a715b76c0b78
Stanislav Zaleski <sigmaray@gmail.com>
2023-09-05 21:00:47 +0600

    .

--- main.cpp
+++ main.cpp
@@ -32,13 +32,40 @@
 
 #define BOARD_SIZE 5
 
-void addCheckboxes(QWidget *window) {
+
+class Win : public QWidget {
+
+ public:
+     Win(QWidget *parent = nullptr);
+};
+
+Win::Win(QWidget *parent)
+    : QWidget(parent) {
+
+//  auto *frame1 = new QFrame(this);
+//  frame1->setFrameStyle(QFrame::Box);
+//  frame1->setCursor(Qt::SizeAllCursor);
+
+//  auto *frame2 = new QFrame(this);
+//  frame2->setFrameStyle(QFrame::Box);
+//  frame2->setCursor(Qt::WaitCursor);
+
+//  auto *frame3 = new QFrame(this);
+//  frame3->setFrameStyle(QFrame::Box);
+//  frame3->setCursor(Qt::PointingHandCursor);
+
+//  auto *grid = new QGridLayout(this);
+//  grid->addWidget(frame1, 0, 0);
+//  grid->addWidget(frame2, 0, 1);
+//  grid->addWidget(frame3, 0, 2);
+
+//  setLayout(grid);
 
     auto *vbox = new QVBoxLayout();
 //    vbox->setSpacing(1);
 
 //    QCheckBox c("1", &window);
-    window->setLayout(vbox);
+    this->setLayout(vbox);
 
 //    auto *settings = new QPushButton("Settings", window);
 //    vbox->addWidget(settings);
@@ -53,11 +80,34 @@ void addCheckboxes(QWidget *window) {
     }
 }
 
+
+//void addCheckboxes(QWidget *window) {
+
+//    auto *vbox = new QVBoxLayout();
+////    vbox->setSpacing(1);
+
+////    QCheckBox c("1", &window);
+//    window->setLayout(vbox);
+
+////    auto *settings = new QPushButton("Settings", window);
+////    vbox->addWidget(settings);
+
+//    for (int y = 0; y < BOARD_SIZE; y++) {
+//        auto *hl = new QHBoxLayout();
+//        for (int x = 0; x < BOARD_SIZE; x++) {
+//            auto *cb = new QCheckBox("x: " + QString::number(x) + "| y: " +QString::number(y));
+//            hl->addWidget(cb);
+//        }
+//        vbox->addLayout(hl);
+//    }
+//}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
-    QWidget window;
+//    QWidget window;
+    Win window;
     window.resize(200, 120);
 
 //    QPushButton quit("Quit", &window);
@@ -65,7 +115,7 @@ int main(int argc, char *argv[])
 //    quit.setGeometry(10, 40, 180, 40);
 //    QObject::connect(&quit, SIGNAL(clicked()), &app, SLOT(quit()));
 
-    addCheckboxes(&window);
+//    addCheckboxes(&window);
 
     window.show();
     return app.exec();
