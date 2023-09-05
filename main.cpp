3deb2c96f45b0310306295fea3d8d1411a1a404a
Stanislav Zaleski <sigmaray@gmail.com>
2023-09-05 20:12:12 +0600

    .

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
-#include "mainwindow.h"
+//#include "mainwindow.h"
+
+//#include <QApplication>
+
+//int main(int argc, char *argv[])
+//{
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+//    return a.exec();
+//}
 
 #include <QApplication>
+#include<QPushButton>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QPushButton HelloWorld("Hello World");
+    HelloWorld.resize(300,60);
+    HelloWorld.show();
     return a.exec();
 }
